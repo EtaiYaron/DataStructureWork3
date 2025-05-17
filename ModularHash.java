522809c75ecdeb05ac7a879219b9646baa8ed927
Etai <yaronet@post.bgu.ac.il>
2025-05-17 14:08:56 +0300

    C:/Program Files/Git/

--- ModularHash.java
+++ ModularHash.java
@@ -21,15 +21,19 @@ public class ModularHash implements HashFactory<Integer> {
         final private int m;
 
         public Functor(int k){
-            m = (int) Math.pow(2,k);
-            a = rand.nextInt(0);
-            b = rand.nextInt(0);
-            p = utils.genPrime(Integer.MAX_VALUE+1, Long.MAX_VALUE);
+            m = (int) Math.pow(2, k);
+            p = utils.genPrime((long)Integer.MAX_VALUE + 1, Long.MAX_VALUE);
+            a = rand.nextInt(Integer.MAX_VALUE - 1) + 1;
+            b = rand.nextInt(Integer.MAX_VALUE);
         }
 
         @Override
         public int hash(Integer key) {
-            return (int) utils.mod(((long) utils.mod((a * key + b), p)) , ((long) m));
+            long ax = ((long)a) * key;
+            long sum = ax + b;
+            long modP = utils.mod(sum, p);
+            int result = (int) utils.mod(modP, (long) m);
+            return result;
         }
 
         public int a() {
